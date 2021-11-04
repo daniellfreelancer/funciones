92076a3431941f298861faf6cb9334980269d04e
daniellfreelancer <90514127+daniellfreelancer@users.noreply.github.com>
2021-11-03 23:25:23 -0300

    Actualización  desafio #4

--- js/javaScript.js
+++ js/javaScript.js
@@ -1,37 +1,3 @@
-// Desafio #1 JS
-
-// var cliente = prompt("Bienvenidos a Sofia Cocina ¿ Cuál es tu nombre ?");
-
-// alert ("Bienvenido " + cliente + " " +"a nuestra tienda, es un gusto tenerte como cliente");
-
-
-// Desafio #2 JS
-
-
-// let userAdmin = prompt("Bienvenido: ingrese usuario");
-// let userPass = prompt("Bienvenido: ingrese su clave");
-
-// if ((userAdmin == "admin") && (userPass == "admin")) {
-//     alert ( "Bienvenido administrador" )
-// } else {
-//     alert ( "Usuario o clave incorrecta")
-// }
-
-
-// Desafio #3 JS
-// Ciclo de 5 en 5
-// for(let i = 0; i < 30 ; i+=5) {
-    
-//     console.log(i);
-//   if (i == 30) {
-//     break;
-//   }
-// } 
-
-// console.log("Es un ciclo de 5 en 5");
-
-// ------------------------------------------------------------------------------
-
 // EJEMPLO #1 DESAFIO 4
 
 /*
